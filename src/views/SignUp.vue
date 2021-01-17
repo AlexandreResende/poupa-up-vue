558dcc36c61ee921de353d096820f6e0c4fe895a
Alexandre Resende <alexandre.amres@gmail.com>
2021-01-16 21:23:12 -0300

    chore: Add vue router and signup view

---
+++ src/views/SignUp.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id='container'>
+    <div id='left-container'>
+      <h1 id='header'>Create your account</h1>
+      <label>Username</label>
+      <input type='text' name='username' id='username' placeholder='Type your username here'><br><br>
+      <label>Password</label>
+      <input type='password' name='password' id='password'><br>
+      <label>Confirm password</label>
+      <input type='password' name='password_confirmation' id='password'><br>
+      <input type='button' id='buttons' value='Submit' name='submit'>
+    </div>
+    <div id='right-container'>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp',
+  data() {
+    return {
+      username: '',
+      password: '',
+      passwordConfirmation: ''
+    }
+  }
+}
+</script>
+
+<style>
+#container {
+  position: absolute;
+  width: 99.5%;
+  height: 100%;
+  overflow: hidden;
+}
+
+#left-container {
+  position: absolute;
+  width: 40%;
+  height: 100%;
+  margin-top: 15%;
+}
+
+#right-container {
+  position: absolute;
+  width: 60%;
+  height: 100%;
+  margin-left: 40%;
+  background-image: url('../assets/finance-poupa-up.jpg');
+  background-size: cover;
+  opacity: 0.7;
+}
+
+input {
+  width: 20rem;
+  height: 3rem;
+  margin: 40px 0 0;
+  border-radius: 1rem;
+}
+
+#buttons {
+  width: 15rem;
+  height: 3rem;
+  margin: 2rem 1rem 0rem 0rem;
+  font-size: 1rem;
+}
+
+#username, #password {
+  width: 20rem;
+  height: 2rem;
+}
+
+#username {
+  margin: 1rem 1rem 1rem 1rem;
+}
+
+#password {
+  margin: 1rem 1rem 1rem 1rem;
+}
+
+a {
+  color: #FFFFFF;
+}
+</style>
