3dd16e50290b69005fb746e2a9253a6ca35ee08e
Alexandre Resende <alexandre.amres@gmail.com>
2021-02-05 21:22:23 -0300

    chore: Create Home view for logged user

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id='container'>
+    <Aside></Aside>
+    <div id='content'>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Aside from '../components/Aside'
+
+export default {
+  components: { Aside },
+  name: 'Home',
+  data() {},
+  methods: {}
+}
+</script>
+
+<style>
+html, body {
+  margin: 0;
+}
+
+#aside {
+  position: absolute;
+  height: 100%;
+  width: 15%;
+  background-color: #000000;
+  opacity: 50%;
+}
+</style>
