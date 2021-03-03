9bebf3fba99d0e87ee77845f30d51cee73f60bed
Alexandre Resende <alexandre.amres@gmail.com>
2021-03-02 22:54:56 -0300

    component: Create Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    id='buttons'
+  >{{ buttonName }}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    buttonName: {
+      type: String,
+      required: true,
+      default: 'Missing value'
+    }
+  },
+}
+</script>
+
+<style>
+#buttons {
+  width: 15rem;
+  height: 3rem;
+  margin: 2rem 1rem 0rem 0rem;
+  font-size: 1rem;
+}
+</style>
